a2f367ef32f2006bdd278f29267447f6c9acd106
oren12321 <oren12321@gmail.com>
2026-01-31 11:37:33 +0200

    Add rotating logs

--- PostInstall/PostInstall.Run.ps1
+++ PostInstall/PostInstall.Run.ps1
@@ -1,1 +1,3 @@
-& "{InstallDirectory}\PostInstall\PostInstall.Main.ps1" -ComponentsDirectory "{ComponentsDirectory}" *>> "{InstallDirectory}\PostInstall\PostInstall.log"
+. "{InstallDirectory}\PostInstall\Utils\Logging.ps1"
+
+& "{InstallDirectory}\PostInstall\PostInstall.Main.ps1" -ComponentsDirectory "{ComponentsDirectory}" *>&1 | Add-RotatingLog -Path "{InstallDirectory}\PostInstall\PostInstall.log" -MaxSize 5MB -MaxFiles 5
--- PostInstall/Utils/Logging.ps1
+++ PostInstall/Utils/Logging.ps1
@@ -36,3 +36,40 @@ function Format-Line {
 
     "$levelPad | $funcPad | $filePad | $Message"
 }
+
+function Add-RotatingLog {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        
+        [long]$MaxSize = 0, # Recommended value: 5MB
+        [int]$MaxFiles = 0, # Recommended value: 5
+        
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        $Logs
+    )
+    # Append logs to log file
+    Add-Content -Path $Path -Value $Logs
+    
+    # Rotate if needed
+    if ($MaxSize -gt 0) {
+        if ((Test-Path $Path) -and (Get-Item $Path).Length -gt $MaxSize) {
+            $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+            Rename-Item $Path "$Path.$timestamp"
+        }
+    }
+    
+    # Enforce retention
+    if ($MaxFiles -gt 0) {
+        $directory = Split-Path $Path
+        $filename = $(Split-Path $Path -Leaf)
+        $files = Get-ChildItem $directory -Filter "$filename.*" `
+            | Where-Object { $_.Name -match '\.\d{8}_\d{6}$' } `
+            | Sort-Object LastWriteTime -Descending
+
+        if ($files.Count -gt $MaxFiles) {
+            $files | Select-Object -Skip $MaxFiles | Remove-Item -Force
+        }
+    }
+}
