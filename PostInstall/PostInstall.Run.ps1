7f050557ca5d4a723712fb2bdf75f2754f2869c3
oren12321 <oren12321@gmail.com>
2026-02-08 23:08:36 +0200

    Relocated PostInstall to Tappet repo

--- PostInstall/PostInstall.Run.ps1
+++
@@ -1,3 +0,0 @@
-. "{InstallDirectory}\PostInstall\Utils\Logging.ps1"
-
-& "{InstallDirectory}\PostInstall\PostInstall.Main.ps1" -ComponentsDirectory "{ComponentsDirectory}" *>&1 | Add-RotatingLog -Path "{InstallDirectory}\PostInstall\PostInstall.log" -MaxSize 5MB -MaxFiles 5
